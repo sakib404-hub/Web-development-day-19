3ae0a4a901e8ef94f0de85adac375205527c1f40
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-08-08 18:13:53 +0600

    function

---
+++ function.js
@@ -0,0 +1,11 @@
+function Fishfries(fish, vegies, masalas) {
+  console.log("Tel Gorom Korun");
+  console.log("Ebar Tel e mosla Din : ", masalas);
+  console.log("Ebar Tel e sobji din : ", vegies);
+  console.log("Ebar Mach din : ", fish);
+
+  return "Yummy FishFries is ready";
+}
+
+let result = Fishfries("Ilish", "Potato", "Chili");
+console.log(result);
